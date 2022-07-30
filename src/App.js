e1f7ef41cb2214a517ee023b15c5f7ac0cac9cde
hiroyuki.obinata <hiroyuki.obinata@gmail.com>
2022-07-30 10:33:56 +0900

    remove unused import

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-import { Amplify } from 'aws-amplify';
 
 import { withAuthenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css';
-
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
 
 function App({ signOut, user }) {
   return (
